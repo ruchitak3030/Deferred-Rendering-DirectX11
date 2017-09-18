afc9b4c79fbe0b1616eaa3c1d74d2a7dfe002f7e
Ruchita Kondvilkar <ruchi3030@outlook.com>
2017-09-17 23:40:30 -0400

    Multiple lights started

--- Game.h
+++ Game.h
@@ -90,5 +90,68 @@ private:
 	// Keeps track of the old mouse position.  Useful for 
 	// determining how far the mouse moved in a single frame.
 	POINT prevMousePos;
+
+	//Per-vertex Data
+	struct VertexPosNormTex
+	{
+		DirectX::XMFLOAT3 Position;
+		DirectX::XMFLOAT3 Normal;
+		DirectX::XMFLOAT2 Tex;
+	};
+
+	//Per-instance data
+	_declspec(align(16)) struct PlaneInstanceData
+	{
+		XMMATRIX WorldMatrix;
+		XMMATRIX InverseTransposeWorldMatrix;
+	};
+
+	struct PerFrameConstantBufferData
+	{
+		XMMATRIX ViewProjectionMatrix;
+	};
+
+	// This structure is used in the simple vertex shader.
+	struct PerObjectConstantBufferData
+	{
+		XMMATRIX WorldMatrix;
+		XMMATRIX InverseTransposeWorldMatrix;
+		XMMATRIX WorldViewProjectionMatrix;
+	};
+
+	struct _Material
+	{
+		_Material()
+			: Emissive(0.0f, 0.0f, 0.0f, 1.0f)
+			, Ambient(0.1f, 0.1f, 0.1f, 1.0f)
+			, Diffuse(1.0f, 1.0f, 1.0f, 1.0f)
+			, Specular(1.0f, 1.0f, 1.0f, 1.0f)
+			, SpecularPower(128.0f)
+			, UseTexture(false)
+		{}
+
+		DirectX::XMFLOAT4   Emissive;
+		//----------------------------------- (16 byte boundary)
+		DirectX::XMFLOAT4   Ambient;
+		//----------------------------------- (16 byte boundary)
+		DirectX::XMFLOAT4   Diffuse;
+		//----------------------------------- (16 byte boundary)
+		DirectX::XMFLOAT4   Specular;
+		//----------------------------------- (16 byte boundary)
+		float               SpecularPower;
+		// Add some padding complete the 16 byte boundary.
+		int                 UseTexture;
+		// Add some padding to complete the 16 byte boundary.
+		float                 Padding[2];
+		//----------------------------------- (16 byte boundary)
+	}; // Total:                                80 bytes (5 * 16)
+
+	struct MaterialProperties
+	{
+		_Material   Material;
+	};
+
+	std::vector<MaterialProperties> materialProperties;
+	int NumInstances;
 };
 
--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -9,8 +9,8 @@ cbuffer externalData : register(b0)
 struct VertexShaderInput
 { 
 	float3 position		: POSITION;
-	float2 uv			: TEXCOORD;
 	float3 normal		: NORMAL;
+	float2 uv			: TEXCOORD;
 	float3 tangent		: TANGENT;
 };
 
@@ -18,9 +18,10 @@ struct VertexToPixel
 {
 	float4 position		: SV_POSITION;
 	float3 normal		: NORMAL;
+	float2 uv			: TEXCOORD;
 	float3 tangent		: TANGENT;
 	float3 worldPos		: POSITION;
-	float2 uv			: TEXCOORD;
+	
 };
 
 
